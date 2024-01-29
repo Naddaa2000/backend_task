23ff5b6dcd359d3eb20b526117b86379b8a904af
Naddaa2000 <148966024+Naddaa2000@users.noreply.github.com>
2024-01-29 12:37:56 +0500

    Update seeder.ts

--- src/users/Seeder/seeder.ts
+++ src/users/Seeder/seeder.ts
@@ -2,18 +2,19 @@ import { seeder } from 'nestjs-seeder';
 import { UserSeeder } from './user.seeder';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user.entity/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 seeder({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'pass@123N',
-      database: 'my_nestjs_project',
+      type: process.env.DB_TYPE as 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      synchronize: process.env.DB_SYNCHRONIZE === 'true',
     }),
     TypeOrmModule.forFeature([User]),
   ],
